698a49ff88a3400c95996077b4ad35f648e77f2f
Yu Min Tan <s10195143@connect.np.edu.sg>
2022-02-07 10:58:17 +0800

    Initial Commit

---
+++ SEAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SEAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
