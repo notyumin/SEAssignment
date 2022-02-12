4697fdcb7b537692ab95c027884db61f6ca6ff29
Zhao Yi Yap <s10196983@connect.np.edu.sg>
2022-02-12 23:42:35 +0800

    Slight fix to ride requested state

--- SEAssignment/RideRequestedState.cs
+++ SEAssignment/RideRequestedState.cs
@@ -61,6 +61,11 @@ public class RideRequestedState : RideState
         }
 
         ride.setState(ride.CustomerCancelledState); // observer pattern
+
+        // implement email receipt system
+
+        ride.Receipt.Status = "Refunded";
+        Console.WriteLine("Ride has been cancelled");
     }
 
     public override void rateCustomer()
